9935f3fbfb399df1031b3cc5ec890d41eb9c8e7e
Andorask <andrewpadgett94@gmail.com>
2020-08-16 00:41:35 -0700

    Made some changes to the html and javascript files

--- script.js
+++ script.js
@@ -1,96 +1,83 @@
 $(document).ready(function () {
 
-    var cityName, cityId, country, temp, tempF, humidity, windSpeed, icon, lon, lat, dt, currentDate, cnt, uvIndex;
-    var cityFound = false;
+    var cityName,
+    var cityId, 
+    var country, 
+    var temp, 
+    var tempFarenheit, 
+    var windSpeed,
+    var humidity,  
+    var icon, 
+    var lon, 
+    var lat, 
+    var dt, 
+    var currentDate, 
+    var cnt, 
+    var uvIndex;
+    var cityFinder = false;
     var initGetLocation = true;
     var apiKey = "857ea1e75f5fbb6b1fae90916208dc3b";
     init();
 
     function init() {
-         getLocation();
-         getFromLocaleStorage();
+         findLocation();
     }
 
-    function getLocation() {
-         if (navigator.geolocation) {
-              navigator.geolocation.getCurrentPosition(showPosition);
-         } else {
-              alert("Geolocation is not supported by this browser.");
-         }
-    }
-
-    function showPosition(position) {
-         lat = position.coords.latitude;
+    function showCoordinates(position) {
          lon = position.coords.longitude;
-         displayLocation(lat, lon)
+         lat = position.coords.latitude;
+         showLocation(lat, lon)
     }
 
-    function displayLocation(latitude, longitude) {
+    function findLocation() {
+     if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(showPosition);
+     } else {
+          alert("Geolocation must be allowed.");
+          }
+     }
+
+    function showLocation(latitude, longitude) {
          var queryURL = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&lat=${latitude}&lon=${longitude}`;
          $.ajax({
               url: queryURL,
               method: "GET"
          }).then(function (response) {
               $("#search").val(response.name);
-              getWeatherDetails();
+              weatherInfo();
          });
     };
 
-    function getWeatherDetails() {
+    function weatherInfo() {
          cityName = $("#search").val().trim();
          var queryURLWeather = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
          $.ajax({
               url: queryURLWeather,
               method: "GET",
               success: function(){
-                   cityFound = true;
+                   cityFinder = true;
               }, 
               error: function(){
-                   cityFound = false;
-                   alert("City not found");
+                   cityFinder = false;
+                   alert("City was not found");
                    $("#search").val("");
               }     
               }).then(function (response) {
+                   temp = response.main.temp;
                    cityId = response.id;
                    country = response.sys.country;
-                   temp = response.main.temp;
-                   tempF = ((temp - 273.15) * 1.80 + 32).toFixed(2);
-                   humidity = response.main.humidity;
+                   tempFarenheit = ((temp - 273.15) * 1.80 + 32).toFixed(2);
                    windSpeed = response.wind.speed;
+                   humidity = response.main.humidity;
                    icon = response.weather[0].icon;
-                   lon = response.coord.lon;
                    lat = response.coord.lat;
+                   lon = response.coord.lon;
                    dt = response.dt;
                    currentDate = new Date(dt * 1000);
-                   var imageUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`
-                   $("#cityImg").attr("src", imageUrl);
-                   $("#cityName").text(cityName + " " + currentDate.toLocaleDateString());
-                   $("#temp").html("Temperature: " + tempF + " &#8457;");
-                   $("#humidity").text("Humidity: " + humidity + " %");
-                   $("#windSpeed").text("Wind Speed: " + windSpeed + " MPH");
-                   getUVIndex();
-                   if(cityFound == true && initGetLocation == false){
-                        console.log(cityFound);
-                        getIntoSearchHistory();
-                        cityFound = false;
-                   }
               });
          }
 
-    function getUVIndex() {
-                   cnt = 1;
-                   var queryURLUvi = `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}&cnt=${cnt}`;
-                   $.ajax({
-                        url: queryURLUvi,
-                        method: "GET"
-                   }).then(function (response2) {
-                        uvIndex = response2.value;
-                        $("#UVIndex").text("UV Index: " + uvIndex);
-                        get5DayForecast();
-                   });
-              }
-
-    function get5DayForecast() {
+    function fivedayForcast() {
                    cnt = 5;
                    var queryURLForecast = `https://api.openweathermap.org/data/2.5/forecast?id=${cityId}&appid=${apiKey}`;
                    $.ajax({
@@ -107,31 +94,6 @@ $(document).ready(function () {
                              };
                              objArray.push(obj);
                         }
-                        $("#11").text(new Date(objArray[0].dt * 1000).toLocaleDateString());
-                        $("#12").attr("src", `https://openweathermap.org/img/wn/${objArray[0].icon}@2x.png`);
-                        $("#13").html("Temp: " + (((objArray[0].temp - 273.15) * 1.80 + 32).toFixed(2)) + " &#8457;");
-                        $("#14").text("Humidity: " + objArray[0].humidity + " %");
-
-                        $("#21").text(new Date(objArray[1].dt * 1000).toLocaleDateString());
-                        $("#22").attr("src", `https://openweathermap.org/img/wn/${objArray[1].icon}@2x.png`);
-                        $("#23").html("Temp: " + (((objArray[1].temp - 273.15) * 1.80 + 32).toFixed(2)) + " &#8457;");
-                        $("#24").text("Humidity: " + objArray[1].humidity + " %");
-
-                        $("#31").text(new Date(objArray[2].dt * 1000).toLocaleDateString());
-                        $("#32").attr("src", `https://openweathermap.org/img/wn/${objArray[2].icon}@2x.png`);
-                        $("#33").html("Temp: " + (((objArray[2].temp - 273.15) * 1.80 + 32).toFixed(2)) + " &#8457;");
-                        $("#34").text("Humidity: " + objArray[2].humidity + " %");
-
-                        $("#41").text(new Date(objArray[3].dt * 1000).toLocaleDateString());
-                        $("#42").attr("src", `https://openweathermap.org/img/wn/${objArray[3].icon}@2x.png`);
-                        $("#43").html("Temp: " + (((objArray[3].temp - 273.15) * 1.80 + 32).toFixed(2)) + " &#8457;");
-                        $("#44").text("Humidity: " + objArray[3].humidity + " %");
-
-                        $("#51").text(new Date(objArray[4].dt * 1000).toLocaleDateString());
-                        $("#52").attr("src", `https://openweathermap.org/img/wn/${objArray[4].icon}@2x.png`);
-                        $("#53").html("Temp: " + (((objArray[4].temp - 273.15) * 1.80 + 32).toFixed(2)) + " &#8457;");
-                        $("#54").text("Humidity: " + objArray[4].humidity + " %");
                    });
               }
-
 });
